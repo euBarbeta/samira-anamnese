a3ceb9631fa6d1e3a329801ae2cef34e3d0fe7d3
euBarbeta <miguelcarvalhobarbeta@gmail.com>
2026-09-22 18:00:38 -0300

    fundo lavanda

--- android/app/src/main/java/com/samira/estetica/MainActivity.java
+++ android/app/src/main/java/com/samira/estetica/MainActivity.java
@@ -1,6 +1,7 @@
 package com.samira.estetica;
 
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.app.NotificationChannelCompat;
 import androidx.core.app.NotificationManagerCompat;
 import com.getcapacitor.BridgeActivity;
@@ -8,6 +9,9 @@ import com.getcapacitor.BridgeActivity;
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        // ✅ Força modo CLARO sempre, ignorando o modo escuro do sistema
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
         super.onCreate(savedInstanceState);
         criarCanalNotificacao();
     }
